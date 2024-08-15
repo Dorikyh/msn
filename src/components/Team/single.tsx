48c1b3481f3862fa29589bdb262bd454a55042a8
Dorikyh🍃 <80162941+Dorikyh@users.noreply.github.com>
2024-08-15 22:36:13 +0000

    ARREGLADO

--- src/components/Team/single.tsx
+++ src/components/Team/single.tsx
@@ -1,22 +1,14 @@
 import { Testimonial } from "@/types/testimonial";
 import Image from "next/image";
+
 const starIcon = (
   <svg width="18" height="16" viewBox="0 0 18 16" className="fill-current">
     <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z" />
   </svg>
 );
 
-const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
-  const { star, name, image, content, designation } = testimonial;
-
-  let ratingIcons = [];
-  for (let index = 0; index < star; index++) {
-    ratingIcons.push(
-      <span key={index} className="text-yellow">
-        {starIcon}
-      </span>,
-    );
-  }
+const Single = ({ testimonial }: { testimonial: Testimonial }) => {
+  const { name, image, designation } = testimonial;
 
   return (
     <div className="w-full">
@@ -37,4 +29,4 @@ const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
   );
 };
 
-export default SingleTestimonial;
+export default Single;
