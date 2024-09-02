85ab0b13f4a43a9629f4c410384127635e8eedfa
Rodsac <147768839+Rodsac@users.noreply.github.com>
2024-09-02 20:24:16 +0000

    feat: mamaguevo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -76,7 +76,7 @@ const Footer = () => {
             <p className="text-center text-base text-body-color dark:text-white">
               Proyecto Desarrollado Por {" "}
               <a
-                href="http://msn-web.vercel.app"
+                href="https://www.instagram.com/msn_fblanca?igsh=MWYxNHBvZ3dpNHZvdA=="
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-primary"
